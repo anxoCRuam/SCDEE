580c9372f35c0d83b6c16ad502e5ec517a1d9ef3
Anxo Canay Reguera <anxo.canay@estudiante.uam.es>
2026-03-11 20:40:29 +0100

    revert(structure): 🚧 re-structuring project

--- LICENSE.md
+++
@@ -1,2 +0,0 @@
-
-    SCDE  © 2025 by Anxo Canay & Alejandro Soler is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
--- backend/scde_backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from api.views import EntityView
-
-
-urlpatterns = [
-    path("<str:entidad>/", EntityView.as_view()),
-    path("<str:entidad>/<str:pk>/", EntityView.as_view()),
-]
